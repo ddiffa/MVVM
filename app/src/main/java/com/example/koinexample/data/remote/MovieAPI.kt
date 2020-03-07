43870088ff103aded45739cf87056a15de293c30
Diffa <ddiffa2@gmail.com>
2020-03-07 14:36:16 +0700

    final

--- app/src/main/java/com/example/koinexample/data/remote/MovieAPI.kt
+++ app/src/main/java/com/example/koinexample/data/remote/MovieAPI.kt
@@ -1,16 +1,12 @@
 package com.example.koinexample.data.remote
 
 import com.example.koinexample.data.model.MovieResponse
-import io.reactivex.Observable
 import io.reactivex.Single
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import java.util.concurrent.TimeUnit
 
 interface MovieAPI {
 
@@ -19,18 +15,8 @@ interface MovieAPI {
 
     companion object Factory {
         fun create(): MovieAPI {
-            val okHttpClient = OkHttpClient.Builder()
-                .connectTimeout(60, TimeUnit.SECONDS)
-                .readTimeout(60, TimeUnit.SECONDS)
-                .addInterceptor(
-                    makeLoggingInterceptor(
-                        true
-                    )
-                )
-                .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl("http://api.themoviedb.org/3/")
-                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
@@ -38,12 +24,6 @@ interface MovieAPI {
             return retrofit.create(MovieAPI::class.java)
         }
 
-        private fun makeLoggingInterceptor(isDebug: Boolean): HttpLoggingInterceptor {
-            val logging = HttpLoggingInterceptor()
-            logging.level =
-                if (isDebug) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
-            return logging
-        }
     }
 
 }
--- app/src/main/java/com/example/koinexample/ui/MovieActivity.kt
+++ app/src/main/java/com/example/koinexample/ui/MovieActivity.kt
@@ -1,13 +1,58 @@
 package com.example.koinexample.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.example.koinexample.R
+import com.example.koinexample.data.local.entity.MovieEntity
+import com.example.koinexample.util.hide
+import com.example.koinexample.util.loadImage
+import com.example.koinexample.util.visible
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+import org.koin.android.architecture.ext.viewModel
 
-class MovieActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity(), MovieContract.View {
+
+    private val viewModel by viewModel<MovieViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.apply {
+            isLoading.observe(this@MovieActivity, Observer<Boolean> { isLoading ->
+                observeLoading(isLoading)
+            })
+            isError.observe(this@MovieActivity, Observer<Throwable> { error ->
+                observeError(error)
+            })
+            movie.observe(this@MovieActivity, Observer<MovieEntity> { movie ->
+                observeNowPlaying(movie)
+            })
+        }
+
+        viewModel.getNowPlayingMovie("ac313fc1138a0ed697567a0dedddc6cd")
+    }
+
+    override fun observeLoading(isLoading: Boolean?) {
+        isLoading?.let {
+            if (it) {
+                progressBar.visible()
+            } else {
+                progressBar.hide()
+            }
+        }
+    }
+
+    override fun observeError(error: Throwable?) {
+        error?.let { toast(it.message.toString()) }
+    }
+
+    override fun observeNowPlaying(movie: MovieEntity) {
+        movie?.let {
+            imagePoster.loadImage("https://image.tmdb.org/t/p/w500/${it.posterPath}")
+            tvTitle.text = it.title
+        }
     }
 }
