d8bb1be51044b84de5bbade8bc7d16a4d299363e
Diffa <ddiffa2@gmail.com>
2020-03-07 12:47:07 +0700

    add module injection

--- app/src/main/java/com/example/koinexample/di/Modules.kt
+++ app/src/main/java/com/example/koinexample/di/Modules.kt
@@ -1,9 +1,29 @@
 package com.example.koinexample.di
 
+import androidx.room.Room
+import com.example.koinexample.data.local.MovieDb
+import com.example.koinexample.data.remote.MovieAPI
+import com.example.koinexample.data.repository.impl.MovieRepositoryImpl
+import com.example.koinexample.ui.MovieInteractor
+import com.example.koinexample.ui.MovieViewModel
+import com.example.koinexample.util.SchedulerProvider
+import org.koin.android.architecture.ext.viewModel
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module.applicationContext
 
 val appModule = applicationContext {
 
+    factory { MovieInteractor(get()) }
+    viewModel { MovieViewModel(get(), get()) }
 
+    bean { MovieAPI.create() }
+    bean { SchedulerProvider() }
+    bean { MovieRepositoryImpl(get(), get()) }
+
+    bean {
+        Room.databaseBuilder(androidApplication(), MovieDb::class.java, "Moviedb").build()
+    }
+
+    bean { get<MovieDb>().movieDao() }
 
 }
